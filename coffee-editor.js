e7532436d062cb36a75678cd7a938b6316264728
David Volm <david@volminator.com>
2013-06-15 21:46:08 -0500

    WIP

--- coffee-editor.js
+++ coffee-editor.js
@@ -16,11 +16,59 @@
         // AMD. Register as an anonymous module.
         define(factory);
     } else {
+        // Class name
+        var CLASS_NAME = 'CoffeeEditor';
+        
         // Browser globals (root is window)
-        root.returnExports = factory();
+        root[CLASS_NAME] = factory();
+        
+        // jQuery sugar
+        root.$.prototype[CLASS_NAME] = function() {
+            var that = $(this),
+                sugar;
+            
+            if(arguments.length > 0) { //need sugar?
+                sugar = [];
+                Array.prototype.slice.call(arguments).forEach(function(v, i, a) {
+                    sugar.push('arguments['+i+']');
+                });
+                
+                return eval('new root[CLASS_NAME](that,' + sugar.join(',')+');');
+            } else {
+                return new root[CLASS_NAME](that);
+            }
+        };
   }
 }(this, function() {
-    var CoffeeEditor;
+    var CoffeeEditor = function(_j) {
+        var el = $(_j).get(0),
+            j = $(el);
+        
+        this.html = j.html();
+        this.el = el;
+        this.j = j;
+        
+        this.attach();
+        
+        el.contentEditable = true;
+    };
+    
+    CoffeeEditor.prototype.attach = function() {
+        var j = this.j;
+        
+        j.find('img').each(function(i, v) {
+            //console.log(v);
+            //WIP use jQuery UI to make resizeable
+        });
+    };
+    
+    CoffeeEditor.prototype.toHTML = function() {
+        return this.html;
+    };
     
     return CoffeeEditor;
 }));
+
+$(function() {
+    window.c = $('.coffee').CoffeeEditor();
+});
